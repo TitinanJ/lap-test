3d35070939643f2617507736378e846ac9780b3c
“parn” <titinanjaem@gmail.com>
2025-07-06 19:01:33 +0700

    feat: configure TypeORM with PostgreSQL and define User entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'users_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
